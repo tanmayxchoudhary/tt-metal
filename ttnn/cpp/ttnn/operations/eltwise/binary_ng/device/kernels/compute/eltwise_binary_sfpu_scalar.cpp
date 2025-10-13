ba4d9c703628b30fac1e0037a4287887e888d899
wenbinlyuTT <wenbinlyu@tenstorrent.com>
2025-10-13 17:33:02 +0000

    My debug traces

--- ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/kernels/compute/eltwise_binary_sfpu_scalar.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/kernels/compute/eltwise_binary_sfpu_scalar.cpp
@@ -22,6 +22,7 @@
 #include "api/compute/isclose.h"
 #include "eltwise_utils_common.hpp"
 #include "eltwise_utils_sfpu.hpp"
+#include "api/debug/dprint.h"
 
 // Process n LHS tiles against a scalar tile at index 0 in cb_post_rhs
 FORCE_INLINE void process_sfpu_scalar_tiles(
@@ -84,6 +85,14 @@ void kernel_main() {
 
     constexpr auto cb_post_lhs = HAS_ACTIVATIONS(LHS) ? tt::CBIndex::c_3 : cb_pre_lhs;
     constexpr auto cb_post_rhs = HAS_ACTIVATIONS(RHS) ? tt::CBIndex::c_4 : cb_pre_rhs;
+    UNPACK(
+        DPRINT << "+KSFPU: nTiles " << num_tiles
+               << " nTiles/cycle " << num_tiles_per_cycle
+               << " CB" << static_cast<int>(cb_post_lhs)
+               << "->dst0 CB" << static_cast<int>(cb_post_rhs)
+               << "->dst1"
+               << ENDL();
+    );
 
     unary_op_init_common(cb_post_lhs, cb_out);
 #ifdef PACK_RELU
