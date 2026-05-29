a23b2f49f2630c2da76d46a11a7d1309068f0613
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-05-29 15:16:23 +0000

    warnings

--- tt_metal/hw/firmware/src/tt-1xx/brisc.cc
+++ tt_metal/hw/firmware/src/tt-1xx/brisc.cc
@@ -187,6 +187,8 @@ void set_deassert_addresses() {
     WRITE_REG(RISCV_DEBUG_REG_TRISC_RESET_PC_OVERRIDE, 0b111);
     WRITE_REG(RISCV_DEBUG_REG_NCRISC_RESET_PC_OVERRIDE, 0x1);
 #else
+    volatile tt_reg_ptr uint32_t* cfg_regs = core.cfg_regs_base(0);
+
     cfg_regs[NCRISC_RESET_PC_PC_ADDR32] = MEM_NCRISC_FIRMWARE_BASE;
     cfg_regs[TRISC_RESET_PC_SEC0_PC_ADDR32] = MEM_TRISC0_FIRMWARE_BASE;
     cfg_regs[TRISC_RESET_PC_SEC1_PC_ADDR32] = MEM_TRISC1_FIRMWARE_BASE;
