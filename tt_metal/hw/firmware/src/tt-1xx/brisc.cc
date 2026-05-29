14d8e44ac94cb4fb9ea885ece10eefa6c33fe674
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-05-29 14:37:35 +0000

    warnings

--- tt_metal/hw/firmware/src/tt-1xx/brisc.cc
+++ tt_metal/hw/firmware/src/tt-1xx/brisc.cc
@@ -179,8 +179,6 @@ void enable_power_management() {
 }
 
 void set_deassert_addresses() {
-    volatile tt_reg_ptr uint32_t* cfg_regs = core.cfg_regs_base(0);
-
 #ifdef ARCH_BLACKHOLE
     WRITE_REG(RISCV_DEBUG_REG_NCRISC_RESET_PC, MEM_NCRISC_FIRMWARE_BASE);
     WRITE_REG(RISCV_DEBUG_REG_TRISC0_RESET_PC, MEM_TRISC0_FIRMWARE_BASE);
